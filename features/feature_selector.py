b1d2fdd11419ee22c827b78f02688ab4b9f0f5fb
BenjaminDHorne <benjamindhorne314@gmail.com>
2018-01-08 15:05:22 -0500

    added feature selection for new communities

--- features/feature_selector.py
+++ features/feature_selector.py
@@ -22,6 +22,8 @@ def feature_select(path):
     features_for_fake = load_features_selected("fake")
     features_for_bias = load_features_selected("bias")
     features_for_newsVSconsp = load_features_selected("newsVSconsp")
+    features_for_newsVSesist = load_features_selected("newsVSesist")
+    features_for_newsVSnewright = load_features_selected("newsVSnewright")
 
     #read in big feature file
     featuresfile = os.path.join(path, "all_features.csv")
@@ -42,9 +44,23 @@ def feature_select(path):
             feats_to_keep_wn = [f for f in feat_names if f in features_for_newsVSconsp]
         ind_to_keep_wn = [feat_names.index(fk) for fk in feats_to_keep_wn]
 
+        # esist
+        if features_for_newsVSesist == 'all':
+            feats_to_keep_esist = feat_names
+        else:
+            feats_to_keep_wn = [f for f in feat_names if f in features_for_newsVSesist]
+        ind_to_keep_esist = [feat_names.index(fk) for fk in feats_to_keep_wn]
+
+        # new_right
+        if features_for_newsVSnewright == 'all':
+            feats_to_keep_newright = feat_names
+        else:
+            feats_to_keep_wn = [f for f in feat_names if f in features_for_newsVSnewright]
+        ind_to_keep_right = [feat_names.index(fk) for fk in feats_to_keep_wn]
+
         #bias
         if features_for_bias == 'all':
-            feats_to_keep_wp = feat_names
+            feats_to_keep_bias= feat_names
         else:
             feats_to_keep_bias = [f for f in feat_names if f in features_for_bias]
         ind_to_keep_bias = [feat_names.index(fk) for fk in feats_to_keep_bias]
@@ -53,12 +69,18 @@ def feature_select(path):
             line_to_keep_fake = []
             line_to_keep_wn = []
             line_to_keep_bias = []
+            line_to_keep_esist = []
+            line_to_keep_newright = []
             line = line.strip().split(",")
             for i in xrange(len(line)):
                 if i in ind_to_keep_fake:
                     line_to_keep_fake.append(line[i])
                 if i in ind_to_keep_wn:
                      line_to_keep_wn.append(line[i])
+                if i in ind_to_keep_esist:
+                     line_to_keep_esist.append(line[i])
+                if i in ind_to_keep_newright:
+                     line_to_keep_newright.append(line[i])
                 if i in ind_to_keep_bias:
                     line_to_keep_bias.append(line[i])
             with open(os.path.join(path, "fake_features.csv"), "w") as out:
@@ -70,5 +92,11 @@ def feature_select(path):
             with open(os.path.join(path, "bias_features.csv"), "w") as out:
                 out.write(",".join(feats_to_keep_bias)+"\n")
                 out.write(",".join(line_to_keep_bias)+"\n")
+            with open(os.path.join(path, "newsVSesist_features.csv"), "w") as out:
+                out.write(",".join(feats_to_keep_esist) + "\n")
+                out.write(",".join(line_to_keep_esist) + "\n")
+            with open(os.path.join(path, "newsVSnewright_features.csv"), "w") as out:
+                out.write(",".join(feats_to_keep_newright) + "\n")
+                out.write(",".join(line_to_keep_newright) + "\n")
         #return top_features
             
